429fd25a36648192fe3273e0cf48fc2d8299a281
Michael Jordan <michdan25@gmail.com>
2021-08-16 22:46:44 +1200

    Metas

---
+++ Prototype2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static GameManager _instance = null;
+    public static GameManager instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject loader = new GameObject();
+                _instance = loader.AddComponent<GameManager>();
+                return loader.GetComponent<GameManager>();
+            }
+            return _instance;
+        }
+    }
+
+    public static bool HasInstance()
+    {
+        return _instance != null;
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+
+        if (_instance == this)
+        {
+            InitialiseFunc();
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Debug.LogWarning("Second Instance of GameManager was created, this instance was destroyed.");
+            Destroy(this);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (_instance == this)
+            _instance = null;
+    }
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void InitialiseFunc()
+    {
+        gameObject.name = "Game Manager";
+    }
+}
---
+++ Prototype2/Assets/Scripts/UI/HUDManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static HUDManager instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.LogError("Second Instance of HUDManager was created, this instance was destroyed.");
+            Destroy(this);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (instance == this)
+            instance = null;
+    }
+
+    #endregion
+
+    [Header("UI Objects")]
+    public GameObject[] m_UIElements;
+
+    private void Start()
+    {
+        gameObject.name = $"HUDManager ({gameObject.name})";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = InputManager.instance.GetMousePositionInScreen();
+        if (InputManager.instance.GetMouseButtonDown(MouseButton.LEFT))
+        {
+            foreach (var elements in m_UIElements)
+            {
+                if(elements.GetComponent<UI_Element>() != null 
+                    && elements.GetComponent<UI_Element>().IsContainingVector(mousePos))
+                {
+                    elements.GetComponent<UI_Element>().OnMouseDownEvent();
+                }
+            }
+        }
+        else if(InputManager.instance.GetMouseButtonUp(MouseButton.LEFT))
+        {
+            foreach (var elements in m_UIElements)
+            {
+                 elements.GetComponent<UI_Element>().OnMouseUpEvent();
+            }
+        }
+    }
+
+    public UI_Element GetElementUnderMouse()
+    {
+        Vector2 mousePos = InputManager.instance.GetMousePositionInScreen();
+        foreach (var elements in m_UIElements)
+        {
+            if (elements.GetComponent<UI_Element>() != null
+                && elements.GetComponent<UI_Element>().IsContainingVector(mousePos))
+            {
+                return elements.GetComponent<UI_Element>();
+            }
+        }
+        return null;
+    }
+
+    public UI_Element GetElementByType(System.Type type)
+    {
+        foreach (var item in m_UIElements)
+        {
+            if(item.GetComponent<UI_Element>().GetType() == type)
+            {
+                return item.GetComponent<UI_Element>();
+            }
+        }
+        return null;
+    }
+}
