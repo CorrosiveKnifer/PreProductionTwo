eace3aece013986898862e71d9059163c8b9c167
Michael Jordan <michdan25@gmail.com>
2021-08-27 09:53:58 +1200

    removed canceling and fixed ranged bug

---
+++ Prototype2/Assets/Scripts/Boss/Boss_Cinematic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Cinematic : MonoBehaviour
+{
+    public Boss_AI m_boss;
+    
+    private bool m_isShowTime = false;
+
+    private void Update()
+    {
+        if(m_isShowTime && !CameraManager.instance.IsDirectorPlaying("BossRoar"))
+        {
+            m_boss.WakeUp();
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            CameraManager.instance.PlayDirector("BossRoar");
+            m_isShowTime = true;
+        }
+    }
+}
