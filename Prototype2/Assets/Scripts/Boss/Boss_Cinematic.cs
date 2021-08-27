a42734345c4d236aba55fdbcf9c9e1831d57b8e5
Michael Jordan <michdan25@gmail.com>
2021-08-27 16:44:51 +1200

    idk

--- Prototype2/Assets/Scripts/Boss/Boss_Cinematic.cs
+++ Prototype2/Assets/Scripts/Boss/Boss_Cinematic.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Boss_Cinematic : MonoBehaviour
 {
     public Boss_AI m_boss;
-    
+    public SoloAudioAgent m_mainGameMusic;
+    public SoloAudioAgent m_bossMusic;
+
     private bool m_isShowTime = false;
     private PlayerController player;
     private void Update()
@@ -26,6 +28,8 @@ public class Boss_Cinematic : MonoBehaviour
             player.m_functionalityEnabled = false;
             player.m_cameraController.m_camera.m_XAxis.Value = 270f;
             player.m_cameraController.m_camera.m_YAxis.Value = 0.5f;
+            m_mainGameMusic.PauseWithFadeOut(0.5F);
+            m_bossMusic.PlayWithFadeIn(1.5f);
             m_isShowTime = true;
         }
     }
